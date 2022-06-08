4001674667e3b716f060424985095c4f424aaa04
WairimuMaringa <makena96wairimu@gmail.com>
2022-06-08 04:18:07 -0700

    Advanced task five

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - determine first ninety eight fibonacci numbers start with one and two
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	int x = 2;
+
+	float a = 1;
+
+	float b = a + 1;
+
+	float c = a + b;
+
+	printf("%.0f, ", a);
+	printf("%.0f, ", b);
+	while (x < 98)
+	{
+		x++;
+		printf("%.0f", c);
+		a = b;
+		b = c;
+		c = a + b;
+		if (x < 98)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
