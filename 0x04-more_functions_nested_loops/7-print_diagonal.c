1066635843b28e66f97b795730589e50ef2c72c4
WairimuMaringa <makena96wairimu@gmail.com>
2022-06-09 09:14:10 -0700

    Task seven

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,11 +9,17 @@
 
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i < n; i++)
 	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(32);
+		}
 		_putchar(92);
+		_putchar(10);
 	}
-	_putchar(10);
+	if (n <= 0)
+		_putchar(10);
 }
