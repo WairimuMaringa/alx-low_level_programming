37d8c5581f5f35a4500a419a2907896ade0872fd
WairimuMaringa <makena96wairimu@gmail.com>
2022-06-09 08:06:09 -0700

    Task five

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - print ten times the numbers zero to fourteen
+ *
+ * Return: Always 0
+ */
+void more_numbers(void)
+{
+	int a, b;
+
+		for (a = 0; a < 10; a++)
+		{
+			for (b = 0; b <= 14; b++)
+			{
+				if (b < 9)
+				{
+					_putchar((b / 10) + 48);
+				}
+				_putchar((b % 10) + 48);
+			}
+			_putchar('\n');
+		}
+}
