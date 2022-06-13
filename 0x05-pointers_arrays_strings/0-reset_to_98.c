2b997244852574bbe62abae7dd6234333cc0cc15
WairimuMaringa <makena96wairimu@gmail.com>
2022-06-13 04:18:43 -0700

    Task zero

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to integer and updates
+ * @n: integer pointer variable
+ *
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
