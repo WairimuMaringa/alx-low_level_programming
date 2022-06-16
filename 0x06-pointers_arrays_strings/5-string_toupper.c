01880f6c6b51c4048fcb421ddeb3c4533e15728b
WairimuMaringa <makena96wairimu@gmail.com>
2022-06-16 00:34:47 -0700

    Task five

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - change lowercase letters of a string to uppercase
+ * @s: string to be converted
+ *
+ * Return: uppercase letters
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] -= 32;
+		}
+	}
+	return (s);
+}
