e189296ae8c67ca2c7436fdf88d4094aef8ae300
WairimuMaringa <makena96wairimu@gmail.com>
2022-06-24 13:16:12 -0700

    Task four

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - program that adds positive numbers
+ * @argc: size array argv
+ * @argv: array or parameters
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int x, y, sum = 0;
+
+	for (x = 1; x < argc; x++)
+	{
+		for (y = 0; *(argv[x] + y); y++)
+		{
+			if (!isdigit(argv[x][y]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[x]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
