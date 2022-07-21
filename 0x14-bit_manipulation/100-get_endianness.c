0e8eb012d2b1bdfd2a8c16c2614ab64eb196cfc8
WairimuMaringa <makena96wairimu@gmail.com>
2022-07-21 12:17:13 -0700

    Advanced task one

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks for endianness meaning the order of
+ * storing and reading of bytes in a memory
+ *
+ * Return: 0 if big endian, 1 if small endian
+ */
+
+int get_endianness(void)
+{
+	unsigned int i;
+	char *c;
+
+	i = 0;
+	c = (char *)&i;
+
+	if (*c)
+		return (1);
+	return (0);
+}
