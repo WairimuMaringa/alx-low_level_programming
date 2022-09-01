e06df95aa8581c9273babde9e070919d1ecaff2b
WairimuMaringa <makena96wairimu@gmail.com>
2022-09-01 04:40:47 -0700

    task four

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -8,8 +8,15 @@
 
 void free_dlistint(dlistint_t *head)
 {
+	dlistint_t *tmp;
+
 	if (head != NULL)
+		while (head->prev != NULL)
+			head = head->prev;
+
+	while ((tmp = head) != NULL)
 	{
-		free_dlistint(head->next);
+		head = head->next;
+		free(tmp);
 	}
 }
