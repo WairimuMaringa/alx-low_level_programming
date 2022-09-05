212ba5688939bb81eb8ebdf993eaf49bbc0132d0
WairimuMaringa <makena96wairimu@gmail.com>
2022-09-05 09:51:31 -0700

    advanced one

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,50 @@
+/**
+ * add - addition function
+ * @a: first number
+ * @b: second number
+ * Return: addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subtraction function
+ * @a: first number
+ * @b: second number
+ * Return: subtraction
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiplication function
+ * @a: first number
+ * @b: second number
+ * Return: multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - division function
+ * @a: first number
+ * @b: second number
+ * Return: division
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - modulo function
+ * @a: first number
+ * @b: second number
+ * Return: modulo
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
